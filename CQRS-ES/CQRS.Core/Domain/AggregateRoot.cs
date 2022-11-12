a352c695dbe409a26558d2c8598c49eda4938a29
sigier <sergiigolinka@gmail.com>
2022-11-12 17:44:01 +0200

    AggregateRoot added

---
+++ CQRS-ES/CQRS.Core/Domain/AggregateRoot.cs
@@ -0,0 +1,58 @@
+﻿using System.Reflection;
+using CQRS.Core.Events;
+
+namespace CQRS.Core.Domain;
+
+public abstract class AggregateRoot
+{
+    protected Guid _id;
+    private readonly List<BaseEvent> _changes = new();
+
+    public Guid Id
+    {
+        get { return _id; }
+    }
+
+    public int Version { get; set; } = -1;
+
+    public IEnumerable<BaseEvent> GetUncommittedChanges()
+    {
+        return _changes;
+    }
+
+    public void MarkChangesAsCommitted()
+    {
+        _changes.Clear();
+    }
+
+    private void ApplyChanges(BaseEvent @event, bool isNew)
+    {
+        MethodInfo? method = this.GetType().GetMethod("Apply", new Type[] { @event.GetType() });
+
+        if (method == null)
+        {
+            throw new ArgumentNullException(nameof(method),
+                $"The Apply method was not found for {@event.GetType().Name}");
+        }
+
+        method.Invoke(this, new object[] { @event });
+
+        if (isNew)
+        {
+            _changes.Add(@event);
+        }
+    }
+
+    private void RaiseEvent(BaseEvent @event)
+    {
+        ApplyChanges(@event, true);
+    }
+
+    public void ReplayEvents(IEnumerable<BaseEvent> events)
+    {
+        foreach (BaseEvent item in events)
+        {
+            ApplyChanges(item, false);
+        }
+    }
+}
