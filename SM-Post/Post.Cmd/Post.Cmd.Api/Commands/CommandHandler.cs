d7a2607a6a80dd195cabfb50dd36f9c55f251a74
sigier <jotulw@proton.me>
2023-02-17 10:53:31 +0200

    Command Handling added

---
+++ SM-Post/Post.Cmd/Post.Cmd.Api/Commands/CommandHandler.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CQRS.Core.Handlers;
+using Post.Cmd.Domain.Aggregates;
+
+namespace Post.Cmd.Api.Commands
+{
+    public class CommandHandler: ICommandHandler
+    {
+        private readonly IEventSourcingHandler<PostAggregate> _eventSourcingHandler;
+
+
+        public CommandHandler(IEventSourcingHandler<PostAggregate> eventSourcingHandler)
+        {
+            _eventSourcingHandler = eventSourcingHandler;
+        }
+
+        public async Task HandleAsync(AddCommentCommand command)
+        {
+            PostAggregate aggregate = await _eventSourcingHandler.GetByIdAsync(command.Id);
+            aggregate.AddComment(command.Comment, command.Username);
+            await _eventSourcingHandler.SaveAsync(aggregate);
+        }
+
+        public async Task HandleAsync(DeletePostCommand command)
+        {
+            PostAggregate aggregate = await _eventSourcingHandler.GetByIdAsync(command.Id);
+            aggregate.DeletePost(command.Username);
+            await _eventSourcingHandler.SaveAsync(aggregate);
+
+        }
+
+        public async Task HandleAsync(EditCommentCommand command)
+        {
+            PostAggregate aggregate = await _eventSourcingHandler.GetByIdAsync(command.Id);
+            aggregate.EditComment(command.Id, command.Comment, command.Username);
+            await _eventSourcingHandler.SaveAsync(aggregate);
+        }
+
+        public async Task HandleAsync(EditMessageCommand command)
+        {
+            PostAggregate aggregate = await _eventSourcingHandler.GetByIdAsync(command.Id);
+            aggregate.EditMessage(command.Message);
+            await _eventSourcingHandler.SaveAsync(aggregate);
+        }
+
+        public async Task HandleAsync(LikePostCommand command)
+        {
+            PostAggregate aggregate = await _eventSourcingHandler.GetByIdAsync(command.Id);
+            aggregate.LikePost();
+            await _eventSourcingHandler.SaveAsync(aggregate);
+
+        }
+
+        public async Task HandleAsync(NewPostCommand command)
+        {
+            PostAggregate aggregate = new PostAggregate(command.Id, command.Author, command.Message);
+            await _eventSourcingHandler.SaveAsync(aggregate);
+        }
+
+        public async Task HandleAsync(RemoveCommentCommand command)
+        {
+            PostAggregate aggregate = await _eventSourcingHandler.GetByIdAsync(command.Id);
+            aggregate.RemoveComment(command.CommentId, command.Username);
+            await _eventSourcingHandler.SaveAsync(aggregate);
+        }
+    }
+}
---
+++ SM-Post/Post.Cmd/Post.Cmd.Api/Commands/ICommandHandler.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Threading.Tasks;
+
+namespace Post.Cmd.Api.Commands
+{
+    public interface ICommandHandler
+    {
+        Task HandleAsync(AddCommentCommand command);
+        Task HandleAsync(DeletePostCommand command);
+        Task HandleAsync(EditCommentCommand command);
+        Task HandleAsync(EditMessageCommand command);
+        Task HandleAsync(LikePostCommand command);
+        Task HandleAsync(NewPostCommand command);
+        Task HandleAsync(RemoveCommentCommand command);
+    }
+}
